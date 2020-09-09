a9633c1907bc3acbc35d66081bf0bd81e2d1d4b5
Kevin Anderson <kmichael419@hotmail.com>
2020-09-09 15:10:39 -0500

    creating POST routes for demographic and social media tables on backend

--- server/routes/form.router.ts
+++ server/routes/form.router.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import express from 'express';
 import pool from '../modules/pool';
+import { ADDRGETNETWORKPARAMS } from 'dns';
 
 const router: express.Router = express.Router();
 
@@ -14,11 +15,8 @@ router.get(
   }
 );
 
-/**
- * POST route template
- */
 router.post(
-  '/register/:id',
+  '/register/about/:id',
   (req: Request, res: Response, next: express.NextFunction): void => {
     console.log(req.body);
     const communityRole = req.body.communityRole;
@@ -28,10 +26,6 @@ router.post(
     const city = req.body.city;
     const state = req.body.state;
     const zipCode = req.body.zipCode;
-    const linkedin = req.body.linkedin;
-    const facebook = req.body.facebook;
-    const instagram = req.body.instagram;
-    const twitter = req.body.twitter;
     const headshotPic = req.body.headshotPic;
     const bio = req.body.bio;
     const tshirtSize = req.body.tshirtSize;
@@ -47,10 +41,6 @@ router.post(
         city,
         state,
         zipCode,
-        linkedin,
-        facebook,
-        instagram,
-        twitter,
         headshotPic,
         bio,
         tshirtSize,
@@ -58,7 +48,61 @@ router.post(
       ])
       .then(() => res.sendStatus(201))
       .catch((err) => {
-        console.log('Error completing about table query', err);
+        console.log('Error completing about table POST query', err);
+        res.sendStatus(500);
+      });
+  }
+);
+
+router.post(
+  '/register/demographic/:id',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    console.log(req.body);
+    const age = req.body.age;
+    const ethnicity = req.body.ethnicity;
+    const gender = req.body.gender;
+    const sexualOrientation = req.body.sexualOrientation;
+    const ability = req.body.ability;
+    const income = req.body.income;
+    const education = req.body.education;
+    const userId = req.params.id;
+
+    const queryText = `INSERT INTO "demographic" (age, ethnicity, gender, sexual_orientation, ability, income, education, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
+    pool
+      .query(queryText, [
+        age,
+        ethnicity,
+        gender,
+        sexualOrientation,
+        ability,
+        income,
+        education,
+        userId,
+      ])
+      .then(() => res.sendStatus(201))
+      .catch((err) => {
+        console.log('Error completing demographic table POST query', err);
+        res.sendStatus(500);
+      });
+  }
+);
+
+router.post(
+  '/register/social/:id',
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    console.log(req.body);
+    const linkedin = req.body.linkedin;
+    const facebook = req.body.facebook;
+    const instagram = req.body.instagram;
+    const twitter = req.body.twitter;
+    const userId = req.params.id;
+
+    const queryText = `INSERT INTO "social_media" (twitter, facebook, linkedin, instagram, user_id) VALUES ($1, $2, $3, $4, $5);`;
+    pool
+      .query(queryText, [linkedin, facebook, instagram, twitter, userId])
+      .then(() => res.sendStatus(201))
+      .catch((err) => {
+        console.log('Error completing social_media table POST query', err);
         res.sendStatus(500);
       });
   }
