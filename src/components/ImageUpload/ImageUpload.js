06869a6b6e59c572a0614de266b09fa321d4bc25
Shelby Cook <shelby.elle.cook@gmail.com>
2020-09-13 17:45:35 -0500

    finishing ImageUpload using s3

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
+import React, { Component } from 'react'; /*8.5K (gzipped: 3.4K)*/
+import { connect } from 'react-redux'; /*13.3K (gzipped: 4.8K)*/
+import DropzoneS3Uploader from 'react-dropzone-s3-uploader'; /*26.5K(gzipped: 8.2K)*/
 
 class ImageUpload extends Component {
   render() {
     const uploadOptions = {
       server: 'http://localhost:5000',
-      //  signingUrlQueryParams: {uploadType: 'avatar'},
     };
 
     const s3Url = 'https://innovateher.s3.amazonaws.com';
