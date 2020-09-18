748e3444651107fe9e8c7d8dcdb309fecd93ccb4
Shelby Cook <shelby.elle.cook@gmail.com>
2020-09-18 12:19:25 -0500

    styling resources page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -15,6 +15,7 @@ import SpaceList from './SpaceList';
 import BusinessList from './BusinessList';
 
 class AboutPage extends Component {
+  state = { defaultModal: false, isOpen: false };
   componentDidMount() {
     this.props.dispatch({
       type: 'FETCH_AIRTABLE_SPEAKER',
@@ -26,9 +27,6 @@ class AboutPage extends Component {
       type: 'FETCH_AIRTABLE_BUSINESSES',
     });
   }
-  state = {
-    heading: 'Airtable test',
-  };
 
   //props.store.imageUrlReducer.avatarPath
   //Add onClick to change state from false to true, const className = object (with styles) = height (150px)
@@ -41,80 +39,146 @@ class AboutPage extends Component {
 
   //<Table hover style={{ wordWrap: 'break-word' }}>
   render() {
-    console.log(typeof this.props.store.speakers.records);
+    //toggleModal = (state) => {
+    //  this.setState({
+    //    [state]: !this.state[state],
+    //  });
+    //};
+    //console.log(typeof this.props.store.speakers.records);
+    //const closedFade = {
+    //  top: '0',
+    //  left: '0',
+    //  backgroundImage: 'linear-gradient(to bottom, transparent, #f2f2f2)',
+    //  transition: 'all 0.3s 0.08s ease-in-out',
+    //};
+
+    //const openFade = {
+    //  top: '0',
+    //  left: '0',
+    //  backgroundImage: 'none',
+    //  transition: 'all 0.3s 0.08s ease-in-out',
+    //};
+
+    //const closedHeight = {
+    //  maxHeight: '150px',
+    //  position: 'relative',
+    //  top: '0',
+    //  bottom: '0',
+    //  overflow: 'hidden',
+    //  zIndex: '0',
+    //  transition: 'all 0.3s 0.08s cubic-bezier(.17,.67,.83,.67)',
+    //};
+
+    //const openHeight = {
+    //  maxHeight: '100%',
+    //  position: 'relative',
+    //  top: '0',
+    //  // bottom: '0',
+    //  overflow: 'hidden',
+    //  zIndex: '999',
+    //  // transform: 'translate(0, 50px)',
+    //  transition: 'all 0.3s 0.08s cubic-bezier(.17,.67,.83,.67)',
+    //};
     return (
-      //<Container>
-      <CardBody>
-        <Container>
+      <Container>
+        {/* <Container className="pt-5">
+          <Card>
+            <CardHeader>
+              <h3 className="display-2">Speakers</h3>
+            </CardHeader>
+            <CardBody>
+              <Card
+                //style={this.state.isOpen ? openHeight : closedHeight}
+                style={{
+                  background: 'linear-gradient(to right, #f2f2f2, #bc91d980)',
+                }}
+              >
+                <Row>
+                  <Col lg={3}>
+                    {this.state.status ? (
+                      <i
+                        style={{ cursor: 'pointer' }}
+                        className="ni ni-fat-delete"
+                      />
+                    ) : (
+                      <i
+                        style={{ cursor: 'pointer' }}
+                        className="ni ni-fat-add"
+                      />
+                    )}
+                  </Col>
+                  <Col lg={4}>NAMES GO HERE</Col>
+                  <Col lg={5}>
+                    <p>DETAILS GO HERE</p>
+                    <p>DETAILS GO HERE</p>
+                    <p>DETAILS GO HERE</p>
+                  </Col>
+                </Row>
+              </Card>
+            </CardBody>
+          </Card>
+        </Container> */}
+
+        <Card
+          style={{ background: 'none', borderRadius: '0px', border: 'none' }}
+          className="mt-5"
+        >
           <CardHeader>
             <h3>Speakers</h3>
           </CardHeader>
-          <Table>
-            {' '}
-            {/*hover*/}{' '}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Contact Info</th>
-              </tr>
-            </thead>
-            {this.props.store &&
-              this.props.store.speakers &&
-              this.props.store.speakers.map((speaker, index) => {
-                return <SpeakerList speaker={speaker} key={index} />;
-              })}
-          </Table>
-        </Container>
-        {/*</CardBody>*/}
-        <Container>
-          {/*<CardBody>*/}
-          <CardHeader>
-            <h2>Spaces</h2>
-          </CardHeader>
-          <Table hover>
-            {' '}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Test</th>
-                <th>Test</th>
-                <th>Test</th>
-              </tr>
-            </thead>
-            {this.props.store &&
-              this.props.store.spaces &&
-              this.props.store.spaces.map((space, index) => {
-                return <SpaceList space={space} key={index} />;
-              })}
-          </Table>
-        </Container>
-        <Container>
-          {/*<CardBody>*/}
-          <CardHeader>
-            <h2>Businesses</h2>
-          </CardHeader>
-          <Table hover>
-            {' '}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Test</th>
-                <th>Test</th>
-                <th>Test</th>
-              </tr>
-            </thead>
-            {this.props.store &&
-              this.props.store.businesses &&
-              this.props.store.spaces.map((business, index) => {
-                return <BusinessList business={business} key={index} />;
-              })}
-          </Table>
-        </Container>
-      </CardBody>
-      //</Container>
+          {this.props.store &&
+            this.props.store.speakers &&
+            this.props.store.speakers.map((speaker, index) => {
+              return <SpeakerList speaker={speaker} key={index} />;
+            })}
+          <Container>
+            {/*<CardBody>*/}
+            <CardHeader>
+              <h2>Spaces</h2>
+            </CardHeader>
+            {/*<Table hover>
+              {' '}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                </tr>
+              </thead>
+              {this.props.store &&
+                this.props.store.spaces &&
+                this.props.store.spaces.map((space, index) => {
+                  return <SpaceList space={space} key={index} />;
+                })}
+            </Table>*/}
+          </Container>
+          <Container>
+            {/*<CardBody>*/}
+            <CardHeader>
+              <h2>Businesses</h2>
+            </CardHeader>
+            {/*<Table hover>
+              {' '}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                  <th>Test</th>
+                </tr>
+              </thead>
+              {this.props.store &&
+                this.props.store.businesses &&
+                this.props.store.spaces.map((business, index) => {
+                  return <BusinessList business={business} key={index} />;
+                })}
+            </Table>*/}
+          </Container>
+        </Card>
+      </Container>
     );
   }
 }
--- src/components/AboutPage/SpeakerList.js
+++ src/components/AboutPage/SpeakerList.js
@@ -9,6 +9,11 @@ import {
   Dropdown,
   DropdownMenu,
   DropdownItem,
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
 } from 'reactstrap';
 
 //import BootstrapTable from 'react-bootstrap-table-next';
@@ -19,66 +24,205 @@ class SpeakerList extends React.Component {
     status: false, //'false' = '+' AND 'true' = '-'
   };
 
-  cellToggle = (event) => {
+  cellToggle = () => {
     this.setState({
       status: !this.state.status,
     });
   };
 
   render() {
+    const closedFade = {
+      top: '0',
+      left: '0',
+      backgroundImage: 'linear-gradient(to bottom, transparent, #f2f2f251)',
+      transition: 'all 0.3s 0.08s ease-in-out',
+    };
+
+    const openFade = {
+      top: '0',
+      left: '0',
+      backgroundImage: 'none',
+      transition: 'all 0.3s 0.08s ease-in-out',
+    };
+
+    const closedHeight = {
+      maxHeight: '50px',
+      position: 'relative',
+      top: '0',
+      bottom: '0',
+      overflow: 'hidden',
+      zIndex: '0',
+      transition: 'all 0.3s 0.08s cubic-bezier(.17,.67,.83,.67)',
+    };
+
+    const openHeight = {
+      maxHeight: '100%',
+      position: 'relative',
+      top: '0',
+      // bottom: '0',
+      overflow: 'hidden',
+      zIndex: '999',
+      // transform: 'translate(0, 50px)',
+      transition: 'all 0.3s 0.08s cubic-bezier(.17,.67,.83,.67)',
+    };
     return (
-      <tbody>
-        <tr>
-          <td onClick={this.cellToggle}>
-            {' '}
-            {this.state.status ? (
-              <i style={{ cursor: 'pointer' }} className="ni ni-fat-delete" />
-            ) : (
-              <i style={{ cursor: 'pointer' }} className="ni ni-fat-add" />
-            )}
-          </td>
-          <td>
-            <Button color="link" size="sm">
-              {this.props.speaker.fields.Name} {` `}
-            </Button>
-          </td>
-          <td>
-            <ul style={{ listStyleType: 'none' }}>
-              <li>
-                <i style={{ cursor: 'pointer' }} className="ni ni-email-83" />
-                {this.props.speaker.fields.Email}
-              </li>
-              <li>
-                <i
-                  style={{ cursor: 'pointer' }}
-                  className="ni ni-mobile-button"
-                />
-                {this.props.speaker.fields['Phone Number']}
-              </li>
-            </ul>
-          </td>
-        </tr>
-        {this.state.status ? (
-          <tr>
-            {' '}
-            {/*Want to add style so when the new row is added, there isn't a line separating the two rows.*/}
-            <td></td>
-            <td colSpan="3">
-              {' '}
-              {this.props.speaker.fields['Speaker Bio']}{' '}
-            </td>{' '}
-            {/*style={{ wordWrap: 'break-word' }}*/}
-            {/*Can't seem to get the text contained. Maybe change table width?*/}
-            <tr>
-              <td>
-                <i class="fa fa-linkedin-square" />
-              </td>
-            </tr>
-          </tr>
-        ) : (
-          ''
-        )}
-      </tbody>
+      <>
+        <Card
+          className="p-3 bg-secondary"
+          style={{
+            // background: 'linear-gradient(to right, #ccace2, #823bae)',
+            // opacity: '0.6',
+            border: 'none',
+          }}
+        >
+          <CardBody style={this.state.status ? openHeight : closedHeight}>
+            <Row style={this.state.status ? openFade : closedFade}>
+              <Col lg={0.5}>
+                {this.state.status ? (
+                  <i
+                    onClick={this.cellToggle}
+                    style={{ cursor: 'pointer' }}
+                    className="ni ni-fat-delete"
+                  />
+                ) : (
+                  <i
+                    onClick={this.cellToggle}
+                    style={{ cursor: 'pointer' }}
+                    className="ni ni-fat-add"
+                  />
+                )}
+              </Col>
+              <Col lg={5}>
+                <div style={{ width: '50%' }}>
+                  {' '}
+                  {this.props.speaker.fields.Name}
+                </div>
+                <p>
+                  {this.props.speaker.fields.Title}
+                  {' at '} {this.props.speaker.fields.Organization}
+                </p>
+                <hr />
+                <div style={{ width: '50%' }}>
+                  {this.props.speaker.fields &&
+                    this.props.speaker.fields['Speaker Photo'] &&
+                    this.props.speaker.fields['Speaker Photo'][0] && (
+                      <img
+                        className="mt-2"
+                        style={{
+                          height: '100%',
+                          width: '100%',
+                          borderRadius: '6px',
+                        }}
+                        src={this.props.speaker.fields['Speaker Photo'][0].url}
+                        alt="speaker headshot"
+                      />
+                    )}
+                </div>
+                {/*<div style={{ width: '50%' }}>
+                  {this.props.speaker.fields['Speaker Photo'][0].url}
+                </div>*/}
+              </Col>
+              <Col lg={4} style={{ borderRight: '1px solid orange' }}>
+                <ul style={{ listStyleType: 'none' }}>
+                  <li className="mb-2">
+                    <i
+                      style={{ cursor: 'pointer' }}
+                      className="ni ni-email-83"
+                    />
+                    {this.props.speaker.fields.Email}
+                  </li>
+                  <li>
+                    <i
+                      style={{ cursor: 'pointer' }}
+                      className="ni ni-mobile-button"
+                    />
+                    {this.props.speaker.fields['Phone Number']}
+                  </li>
+                  <li>
+                    <i style={{ cursor: 'pointer' }} className="ni ni-laptop" />
+                    {this.props.speaker.fields['Website']}
+                  </li>
+                </ul>
+              </Col>
+              <Col lg={1}></Col>
+            </Row>
+            <Row>
+              <Col lg={5}>
+                <div>
+                  <p className="font-weight-light">
+                    {this.props.speaker.fields['Speaker Bio']}
+                  </p>
+                </div>
+              </Col>
+              <Col lg={9} style={{ border: '1px solid orange' }}></Col>
+              <Col lg={1}></Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </>
+      // <tbody>
+      //   <tr>
+      //     <td onClick={this.cellToggle}>
+      //       {' '}
+      //       {this.state.status ? (
+      //         <i style={{ cursor: 'pointer' }} className="ni ni-fat-delete" />
+      //       ) : (
+      //         <i style={{ cursor: 'pointer' }} className="ni ni-fat-add" />
+      //       )}
+      //     </td>
+      //     <td>
+      //       <Button
+      //         color="link"
+      //         size="sm"
+      //         href={this.props.speaker.fields['LinkedIn Profile']}
+      //         target=" "
+      //       >
+      //         {this.props.speaker.fields.Name} {` `}
+      //       </Button>
+      //     </td>
+      //     <td>
+      //       <ul style={{ listStyleType: 'none' }}>
+      //         <li>
+      //           <i style={{ cursor: 'pointer' }} className="ni ni-email-83" />
+      //           {this.props.speaker.fields.Email}
+      //         </li>
+      //         <li>
+      //           <i
+      //             style={{ cursor: 'pointer' }}
+      //             className="ni ni-mobile-button"
+      //           />
+      //           {this.props.speaker.fields['Phone Number']}
+      //         </li>
+      //         <li>
+      //           <i style={{ cursor: 'pointer' }} className="ni ni-laptop" />
+      //           {this.props.speaker.fields['Website']}
+      //         </li>
+      //       </ul>
+      //     </td>
+      //   </tr>
+      //   {this.state.status ? (
+      //     <tr>
+      //       {' '}
+      //       {/*Want to add style so when the new row is added, there isn't a line separating the two rows.*/}
+      //       <td>Random</td>
+      //       <td style={{ overflowWrap: 'break-word', maxWidth: '50px' }}>
+      //         {' '}
+      //         <p style={{ overflowWrap: 'break-word' }}>
+      //           {this.props.speaker.fields['Speaker Bio']}{' '}
+      //         </p>
+      //       </td>{' '}
+      //       random
+      //       {/*Can't seem to get the text contained. Maybe change table width?*/}
+      //       {/* <tr>
+      //         <td>
+      //           <i class="fa fa-linkedin-square" />
+      //         </td>
+      //       </tr> */}
+      //     </tr>
+      //   ) : (
+      //     ''
+      //   )}
+      // </tbody>
     );
   }
 }
